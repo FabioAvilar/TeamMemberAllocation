cf6601940cfc259ab3453637949fdb62868249a3
FabioBassani <avilar.fabio@yahoo.com.br>
2022-11-27 11:40:33 +0000

    update project fase I

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import Header from './Header';
+import Employees from './Employees';
+import Footer from './Footer';
 
 export default function App() {
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
+      <Header />
+      <Employees />
+      <Footer />
     </main>
   )
 }
